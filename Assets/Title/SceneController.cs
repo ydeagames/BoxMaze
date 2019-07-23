08a5897398cafe09f863930e4d6ba4ff3ad93358
Shima-D <shimadaryoya19@gmail.com>
2019-07-23 14:37:27 +0900

    タイトルしーんｎ

---
+++ Assets/Title/SceneController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//ボタンを使用するためUIとSceneManagerを使用ためSceneManagementを追加
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+
+    // ボタンをクリックするとBattleSceneに移動します
+    public void ButtonClicked()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
