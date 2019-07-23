cd3b8bdbfdb28a4f95602c43b573d7e89dd8c467
YdeaGames <ydeagames@gmail.com>
2019-07-23 13:59:00 +0900

    転がり方

--- Assets/CubeBehaviour.cs
+++ Assets/CubeBehaviour.cs
@@ -4,39 +4,77 @@ using UnityEngine;
 
 public class CubeBehaviour : MonoBehaviour
 {
-    bool moving;
+    Vector3 rotatePoint = Vector3.zero;  //回転の中心
+    Vector3 rotateAxis = Vector3.zero;   //回転軸
+    float cubeAngle = 0f;                //回転角度
 
-    // Start is called before the first frame update
+    float cubeSizeHalf;                  //キューブの大きさの半分
+    bool isRotate = false;               //回転中に立つフラグ。回転中は入力を受け付けない
+
     void Start()
     {
-        
+        cubeSizeHalf = transform.localScale.x / 2f;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (!moving)
+        //回転中は入力を受け付けない
+        if (isRotate)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
-                StartCoroutine(Move(transform.position, transform.rotation, transform.position + Vector3.left, Quaternion.Euler(0, 0, 90) * transform.rotation));
-            if (Input.GetKeyDown(KeyCode.RightArrow))
-                StartCoroutine(Move(transform.position, transform.rotation, transform.position + Vector3.right, Quaternion.Euler(0, 0, -90) * transform.rotation));
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-                StartCoroutine(Move(transform.position, transform.rotation, transform.position + Vector3.forward, Quaternion.Euler(90, 0, 0) * transform.rotation));
-            if (Input.GetKeyDown(KeyCode.DownArrow))
-                StartCoroutine(Move(transform.position, transform.rotation, transform.position + Vector3.back, Quaternion.Euler(-90, 0, 0) * transform.rotation));
+            rotatePoint = transform.position + new Vector3(cubeSizeHalf, -cubeSizeHalf, 0f);
+            rotateAxis = new Vector3(0, 0, -1);
         }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rotatePoint = transform.position + new Vector3(-cubeSizeHalf, -cubeSizeHalf, 0f);
+            rotateAxis = new Vector3(0, 0, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rotatePoint = transform.position + new Vector3(0f, -cubeSizeHalf, cubeSizeHalf);
+            rotateAxis = new Vector3(1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            rotatePoint = transform.position + new Vector3(0f, -cubeSizeHalf, -cubeSizeHalf);
+            rotateAxis = new Vector3(-1, 0, 0);
+        }
+        // 入力がない時はコルーチンを呼び出さないようにする
+        if (rotatePoint == Vector3.zero)
+            return;
+        StartCoroutine(MoveCube());
     }
 
-    IEnumerator Move(Vector3 startPos, Quaternion startRot, Vector3 endPos, Quaternion endRot)
+    IEnumerator MoveCube()
     {
-        moving = true;
-        for (float t = 0; t < 1; t += .02f)
+        //回転中のフラグを立てる
+        isRotate = true;
+
+        //回転処理
+        float sumAngle = 0f; //angleの合計を保存
+        while (sumAngle < 90f)
         {
-            transform.position = Vector3.Lerp(startPos, endPos, t);
-            transform.rotation = Quaternion.Slerp(startRot, endRot, t);
+            cubeAngle = 15f; //ここを変えると回転速度が変わる
+            sumAngle += cubeAngle;
+
+            // 90度以上回転しないように値を制限
+            if (sumAngle > 90f)
+            {
+                cubeAngle -= sumAngle - 90f;
+            }
+            transform.RotateAround(rotatePoint, rotateAxis, cubeAngle);
+
             yield return null;
         }
-        moving = false;
+
+        //回転中のフラグを倒す
+        isRotate = false;
+        rotatePoint = Vector3.zero;
+        rotateAxis = Vector3.zero;
+
+        yield break;
     }
 }
