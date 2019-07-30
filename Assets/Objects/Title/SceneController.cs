1233d3e56a999ca0fe744ad09b19c105c21116d3
YdeaGames <ydeagames@gmail.com>
2019-07-30 11:52:56 +0900

    Title

---
+++ Assets/Objects/Title/SceneController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//ボタンを使用するためUIとSceneManagerを使用ためSceneManagementを追加
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // ボタンをクリックするとBattleSceneに移動します
+    public void ButtonClicked()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    // ボタンをクリックすると終了
+    public void ExitClicked()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+		Application.OpenURL("http://www.google.com/");
+#else
+		Application.Quit();
+#endif
+    }
+}
