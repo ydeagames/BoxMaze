edc8fc31c042757f96d87e78b85fb6d1e0f6f9b6
YdeaGames <ydeagames@gmail.com>
2019-12-23 06:10:56 +0900

    WIP

--- Assets/Targetter.cs
+++ Assets/Targetter.cs
@@ -9,7 +9,7 @@ public class Targetter : MonoBehaviour
     Vector3 startPos;
     public FloorBehaviour floor;
 
-    public float controllRadius = .3f;
+    public float controllRadius = .1f;
     public float bias = .2f;
 
     void Start()
@@ -22,45 +22,34 @@ public class Targetter : MonoBehaviour
     {
         var camera = Camera.main;
         var size = floor.settings.size;
-
-        var p1 = camera.WorldToScreenPoint(new Vector2Int(-size.x, +size.y).ToWorldPos());
-        var p2 = camera.WorldToScreenPoint(new Vector2Int(+size.x, +size.y).ToWorldPos());
-        var p3 = camera.WorldToScreenPoint(new Vector2Int(+size.x, -size.y).ToWorldPos());
-        var p4 = camera.WorldToScreenPoint(new Vector2Int(-size.x, -size.y).ToWorldPos());
-
-        var bound = new Bounds(p1, Vector3.zero);
-        bound.Encapsulate(p2);
-        bound.Encapsulate(p3);
-        bound.Encapsulate(p4);
+        var min = -size.ToWorldPos();
+        var max = size.ToWorldPos();
 
         var mouse = camera.ScreenToViewportPoint(Input.mousePosition);
         var uv0 = new Vector2(Mathf.Clamp01(mouse.x), Mathf.Clamp01(mouse.y)); // 0～1
 
-        var uv = uv0; // 0～1
-        uv = uv * 2 - Vector2.one; // -1～1
-        {
-            var trace = (1 < Mathf.Abs(uv.y / uv.x)) ? uv * Mathf.Abs(1 / uv.y) : uv * Mathf.Abs(1 / uv.x);
-            var lengthmax = trace.magnitude; // 0～1.4
-            var length = uv.magnitude; // 0～1.4
-            var t = length / lengthmax; // 0～1
-            t = Mathf.Pow(t, 7); // 0～1
-            length = Mathf.Lerp(0, lengthmax, t); // 0～1.4
-            uv = uv.normalized * length; // -1～1
-        }
-        uv = (uv + Vector2.one) / 2; // 0～1
+        var uv = uv0;
+        var offset = Camera.main.WorldToScreenPoint(transform.position);
+        if (uv.x < controllRadius)
+            offset.x--;
+        if (uv.x > 1 - controllRadius)
+            offset.x++;
+        if (uv.y < controllRadius)
+            offset.y--;
+        if (uv.y > 1 - controllRadius)
+            offset.y++;
 
-        Debug.Log($"UV0:{uv0} UV:{uv} Bound:{bound}");
+        offset.x = Mathf.Clamp(offset.x, min.x, max.x);
+        offset.z = Mathf.Clamp(offset.z, min.z, max.z);
+        offset.y = startPos.y;
 
-        var boundpoint = new Vector3(Mathf.Lerp(bound.min.x, bound.max.x, uv.x), Mathf.Lerp(bound.min.y, bound.max.y, uv.y));
-        var offset = Vector3.zero;
+        //Debug.Log($"UV0:{uv0} UV:{uv} Mouse:{mouse}");
         var plane = new Plane(Vector3.up, Vector3.zero);
-        var ray = Camera.main.ScreenPointToRay(boundpoint);
+        var ray = Camera.main.ScreenPointToRay(offset);
         if (plane.Raycast(ray, out float enter))
         {
-            offset = ray.GetPoint(enter);
+            var pos = ray.GetPoint(enter);
+            transform.position = Vector3.Lerp(transform.position, pos, movePercent);
         }
-        var pos = target.transform.position + offset;
-        pos.y = startPos.y;
-        transform.position = Vector3.Lerp(transform.position, pos, movePercent);
     }
 }
